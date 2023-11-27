8787f845d594677f0a55ea07c5e305c26f145f32
Zhuoyan Xu <zxu68341@icloud.com>
2023-11-26 22:14:34 -0600

    jobarray submit

--- jobArrayScript.sh
+++ jobArrayScript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-#SBATCH -J vary_num  # give the job a name   
+#SBATCH -J eval_res50  # give the job a name   
 #***SBATCH --partition=batch_default ***
 # 
 # 1 node, 1 CPU per node (total 1 CPU), wall clock time of hours
@@ -14,8 +14,8 @@
 #SBATCH --mem=40GB
 #SBATCH -p research
 #SBATCH --exclude=euler[01-09],euler[11-12],euler[14],euler[24-27]
-#SBATCH --error=./eulerlog/ft_array_job_slurm_%A_%a.err
-#SBATCH --output=./eulerlog/ft_array_job_slurm_%A_%a.out
+#SBATCH --error=./eulerlog/res50_job_slurm_%A_%a.err
+#SBATCH --output=./eulerlog/res50_job_slurm_%A_%a.out
 source ~/.bashrc
 
 echo "SLURM_JOBID: " $SLURM_JOBID
@@ -35,10 +35,13 @@ print(torch.cuda.get_device_name(0))
 EOF
 
 echo "======== run with different inputs ========"
-# for take different input from different lines of input_file_list.txt
-# echo $( awk "NR==$SLURM_ARRAY_TASK_ID" input_path_list.txt )
 
-python tools/eval_baseline.py \
+# python tools/eval_baseline.py \
+#     --limit 5000 \
+#     --skip_block $( awk "NR==$SLURM_ARRAY_TASK_ID" input_files_jobarray/input_file_skip_block.txt )
+
+python eval_constrain_macs.py \
+    --load_path './log/train_resnet50_imagenet' \
     --limit 5000 \
     --skip_block $( awk "NR==$SLURM_ARRAY_TASK_ID" input_files_jobarray/input_file_skip_block.txt )
 
